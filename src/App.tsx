2015abd3eaae136f09bdbddbf8d58e641d5fee68
Mukund Vashisht <Mukund.Vashisht@cyncly.com>
2026-02-03 20:05:31 +0530

    Set document title dynamically based on data from JSON

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
 import './App.css'
 import EntryPage from './EntryPage/EntryPage'
+import Data from './Data/data.json'
 
 function App() {
+  useEffect(() => {
+    document.title = `${Data.name} | Portfolio`
+  }, [])
+
   return (
     <>
       <EntryPage />
